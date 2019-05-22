9709999c10d4e1a67f5d0fd4a7ed9c1dcb03b252
IanMcAllister <47300264+IanMcAllister@users.noreply.github.com>
2019-05-22 14:02:04 -0400

    Update Ian's-Creative-Change.js
    object added

--- Ian's-Creative-Change.js
+++ Ian's-Creative-Change.js
@@ -1,1 +1,40 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+
+// Recursion
+// Edited Video: https://www.youtube.com/watch?v=jPsZwrV9ld0
+
+function setup() {
+  createCanvas(600, 600);
+  circ = new circleOBJ();
+}
+
+class circleOBJ{
+    constructor(){
+      this.x = mouseX;
+      this.y = mouseY;
+      this.d = 400
+      
+    }
+  move(){
+    this.x = mouseX;
+    this.y = mouseY;
+    this.d = random(200);
+  }
+  display(){
+    ellipse(this.x,this.y,this.d);    
+}
+
+  
+}
+
+function draw(){
+  circ.display();
+  circ.move();
+  
+  
+}
+
+
 
